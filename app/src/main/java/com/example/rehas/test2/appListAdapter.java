182e0a441ab3fe135447b7a48d2a1ecbf2102faf
rehas <beratson@hotmail.com>
2017-11-27 21:45:02 +0300

    CheckBox addition RecyclerView OnClick to change checkbox RecyclerView prevention from erasing checkboxes by keeping checked array

--- app/src/main/java/com/example/rehas/test2/appListAdapter.java
+++ app/src/main/java/com/example/rehas/test2/appListAdapter.java
@@ -1,9 +1,11 @@
 package com.example.rehas.test2;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -17,10 +19,12 @@ import java.util.List;
 public class appListAdapter extends RecyclerView.Adapter <appListAdapter.appViewHolder> {
 
     private List appList;
+    public static boolean[] checkStatus;
 
     public appListAdapter(List appList) {
         this.appList = appList;
         this.appCount = appList.size();
+        this.checkStatus = new boolean[appCount];
     }
 
     public String appFullName;
@@ -33,11 +37,16 @@ public class appListAdapter extends RecyclerView.Adapter <appListAdapter.appView
                 from(parent.getContext()).
                 inflate(R.layout.appcardview, parent, false);
 
+
+
         return new appViewHolder(appListView);
     }
 
     @Override
     public void onBindViewHolder(appViewHolder appNameHolder, int position) {
+
+
+
         appFullName =  appList.get(position).toString();
         appNameHolder.appFullName.setText(appFullName);
 
@@ -45,6 +54,12 @@ public class appListAdapter extends RecyclerView.Adapter <appListAdapter.appView
                              toString().
                              substring(appFullName.indexOf(' '));
         appNameHolder.appName.setText(appName);
+
+
+
+        appNameHolder.cardCheckBox.setChecked(checkStatus[position] ? true : false);
+
+        //appNameHolder.cardCheckBox.setChecked(false);
 
         //appCount = appList.size();
         //appNameHolder.appCount.setText((String.valueOf(appCount)));
@@ -57,19 +72,39 @@ public class appListAdapter extends RecyclerView.Adapter <appListAdapter.appView
     }
 
 
-    public static class appViewHolder extends RecyclerView.ViewHolder {
+    public static class appViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         protected TextView appName;
         protected TextView appFullName;
+        protected CheckBox cardCheckBox;
+
+
+
         //protected TextView appCount;
 
         public appViewHolder(View itemView) {
             super(itemView);
-
+            //this.setIsRecyclable(false);
+            itemView.setOnClickListener(this);
             appName = (TextView) itemView.findViewById(R.id.appName);
             appFullName = (TextView) itemView.findViewById(R.id.appFullName);
+            cardCheckBox = (CheckBox) itemView.findViewById(R.id.cardViewCheck);
             //appCount = (TextView) itemView.findViewById(R.id.appCount);
 
+        }
+
+
+
+
+        @Override
+        public void onClick(View itemView) {
+
+            cardCheckBox = (CheckBox) itemView.findViewById(R.id.cardViewCheck);
+
+            cardCheckBox.setChecked(cardCheckBox.isChecked()? false : true);
+
+            checkStatus[getLayoutPosition()] = cardCheckBox.isChecked();
+            Log.d("Check Box",  "Clicked!!!! position: " + getLayoutPosition());
         }
     }
 }
